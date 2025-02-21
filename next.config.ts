c71cdf2068e4f9250b887780cfaeabf6856fe64b
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-02-21 07:18:28 +0100

    feat: add remote image pattern configuration for Next.js

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '1xoi7lid5j.ufs.sh',
+        port: '',
+        pathname: '/f/**'
+      }
+    ]
+  }
 }
  
 export default nextConfig
