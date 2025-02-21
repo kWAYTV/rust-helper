2e9f6f0ef9eb6383da105b8b4b37a06e70434e5a
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-02-21 07:55:47 +0100

    chore: match countdown skeleton to actual ui

--- src/components/core/app/rust/countdown/skeleton.tsx
+++ src/components/core/app/rust/countdown/skeleton.tsx
@@ -1,14 +1,22 @@
+import { Card } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 
 export function CountdownSkeleton() {
   return (
-    <div className='flex items-center gap-4'>
-      {[...Array(4)].map((_, i) => (
-        <div key={i} className='flex flex-col items-center gap-2'>
-          <Skeleton className='h-[80px] w-[80px] rounded-lg bg-black/20' />
-          <Skeleton className='h-4 w-12 bg-black/20' />
+    <Card>
+      <div className='flex flex-col space-y-1.5 p-6'>
+        <Skeleton className='mx-auto h-7 w-48' />
+      </div>
+      <div className='p-6 pt-0'>
+        <div className='flex flex-wrap justify-center gap-3 sm:gap-4 md:gap-6'>
+          {[...Array(4)].map((_, i) => (
+            <div key={i} className='flex flex-col items-center gap-2'>
+              <Skeleton className='h-[60px] w-[60px] sm:h-[70px] sm:w-[70px] md:h-[80px] md:w-[80px]' />
+              <Skeleton className='h-4 w-12' />
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
+    </Card>
   );
 }
