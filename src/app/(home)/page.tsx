dd9fb7b52f5dee6949aadca191b2a9831354a455
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-02-21 07:43:41 +0100

    fix: lower the y spacing in homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import { CountdownTimer } from '@/components/core/app/rust/countdown/timer';
 export default function Home() {
   return (
     <div className='w-full max-w-3xl px-4 py-8'>
-      <div className='space-y-32'>
+      <div className='space-y-24'>
         <Hero />
         <CountdownTimer />
       </div>
