690da1976d24ea9a7a8da9e4f4bf1b3f9adf28a3
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-02-21 08:04:40 +0100

    fix: eslint issues

--- src/components/core/app/home/hero.tsx
+++ src/components/core/app/home/hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <div className='text-center'>
       <h1 className='text-4xl font-bold'>Welcome to Rust Helper</h1>
       <p className='text-muted-foreground mt-4'>
-      Your handy Rust game helper for raids and offlines!
+        Your handy Rust game helper for raids and offlines!
       </p>
     </div>
   );
