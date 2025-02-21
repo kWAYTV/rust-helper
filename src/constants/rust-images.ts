000b7564c4e35c5166cb835f94769f5aaead6f4b
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-02-21 07:18:49 +0100

    feat: add rust image consts with name, url and alt

---
+++ src/constants/rust-images.ts
@@ -0,0 +1,276 @@
+import { RustImageCategories } from '@/types/rust-images';
+
+export const rustImages: RustImageCategories = {
+  components: [
+    {
+      name: 'fuse',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbRkXUwrkMeInHPcZ2sQ83oYij91JBNWEldADu',
+      alt: 'Fuse'
+    },
+    {
+      name: 'gear',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbk17ID19oN8MSUrE4Zfu0KTapx3bPgY5LQmXh',
+      alt: 'Gear'
+    },
+    {
+      name: 'metalblade',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbbwkaRwKwhSJe4EIXF5Lq0HgRr2lAxjuVDtBn',
+      alt: 'Metal Blade'
+    },
+    {
+      name: 'metalpipe',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbaPOVn9vuCF7dwP2ilOLhc6MIQtH3Wq81UaAB',
+      alt: 'Metal Pipe'
+    },
+    {
+      name: 'metalspring',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbLUv2qinPqOWxfYU6Et1vseZ3TAGR7zN09duS',
+      alt: 'Metal Spring'
+    },
+    {
+      name: 'propanetank',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbkdNssI29oN8MSUrE4Zfu0KTapx3bPgY5LQmX',
+      alt: 'Propane Tank'
+    },
+    {
+      name: 'riflebody',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbKyZALig0iZ71FNOdljs8CkbxS4rtzGEwm6fP',
+      alt: 'Rifle Body'
+    },
+    {
+      name: 'roadsign',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb4u8hvYRQ81ETnxS0sWdfDHYal5wgpBZMV6tq',
+      alt: 'Road Sign'
+    },
+    {
+      name: 'rope',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbUaDTm5X40uBwLHGjA9OWQxRVm5YbEyz1cJIC',
+      alt: 'Rope'
+    },
+    {
+      name: 'semibody',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb9ePInzNTVFafeJWPDuGqrmwn7Sg36hN8RBQz',
+      alt: 'Semi-Automatic Body'
+    },
+    {
+      name: 'sewingkit',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbFwUKV5B5anjwty3UZS8VdNiJeAlkPEH7I2L9',
+      alt: 'Sewing Kit'
+    },
+    {
+      name: 'sheetmetal',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbyCxhMfwPK3pmj0QMf1IDoPi5sAvzYF8xEJrt',
+      alt: 'Sheet Metal'
+    },
+    {
+      name: 'smgbody',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbo7B0PMaU2Nx8Lr96QosXta5wREjDgIVdeHZb',
+      alt: 'SMG Body'
+    },
+    {
+      name: 'tarp',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbmppQI5NYoyV1QXbJLOv7kqDtzMc5lCUK3fE9',
+      alt: 'Tarp'
+    },
+    {
+      name: 'techtrash',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbaXJH3TvuCF7dwP2ilOLhc6MIQtH3Wq81UaAB',
+      alt: 'Tech Trash'
+    }
+  ],
+  deployables: [
+    {
+      name: 'autoturret',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbptKqvv7wdBj4NVn8XUKLieSovIulTtymAqYb',
+      alt: 'Auto Turret'
+    },
+    {
+      name: 'flameturret',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbQ2dBUkyxngFyj6BJIGT7MPr3fdKlEhiZHvs2',
+      alt: 'Flame Turret'
+    },
+    {
+      name: 'guntrap',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbMdcuCKtFkzwbAr2lMcOV7xtjQ5GvgoXIJiH0',
+      alt: 'Gun Trap'
+    },
+    {
+      name: 'sam',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbvussmo4LJhOdsGDTvbuX25UzlSCF4VxianM1',
+      alt: 'SAM Site'
+    },
+    {
+      name: 'toolcupboard',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb6R7Iy7zoBUeFNDc54MhWR8YEuX6nCxftIGjz',
+      alt: 'Tool Cupboard'
+    },
+    {
+      name: 'vendingmachine',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbGWnShyBVgWseJBKMrTfyZoP5AtwFlYnh1XCk',
+      alt: 'Vending Machine'
+    },
+    {
+      name: 'workbench1',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbaKr4b6vuCF7dwP2ilOLhc6MIQtH3Wq81UaAB',
+      alt: 'Workbench Level 1'
+    },
+    {
+      name: 'workbench2',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbPVYi0TOU8zq5OiBgAwQIHeXsxGSkZTjrnF6D',
+      alt: 'Workbench Level 2'
+    },
+    {
+      name: 'workbench3',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbyCY257cPK3pmj0QMf1IDoPi5sAvzYF8xEJrt',
+      alt: 'Workbench Level 3'
+    }
+  ],
+  doors: [
+    {
+      name: 'floorladder',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbjpCJNaw4hcAX8LH0w1E7FRsn6B9MOpDzYQqb',
+      alt: 'Floor Ladder Hatch'
+    },
+    {
+      name: 'garagedoor',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb29hdhZUE0eMyUA5kNuQ7qFdmf9LOCKGB1Ziv',
+      alt: 'Garage Door'
+    },
+    {
+      name: 'hqdoor',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbk5M5VO9oN8MSUrE4Zfu0KTapx3bPgY5LQmXh',
+      alt: 'High Quality Door'
+    },
+    {
+      name: 'metaldoor',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbDzSFlUrkPn9ZDaz2WwLGKMOiIS3XjETCmq6R',
+      alt: 'Metal Door'
+    },
+    {
+      name: 'woodendoor',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwboLIRGNaU2Nx8Lr96QosXta5wREjDgIVdeHZb',
+      alt: 'Wooden Door'
+    }
+  ],
+  raid: [
+    {
+      name: 'bullets',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb2FQ6HfUE0eMyUA5kNuQ7qFdmf9LOCKGB1Ziv',
+      alt: 'Bullets'
+    },
+    {
+      name: 'c4',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbUEWBg9X40uBwLHGjA9OWQxRVm5YbEyz1cJIC',
+      alt: 'C4 Explosive'
+    },
+    {
+      name: 'gunpowder',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbQdPsUcyxngFyj6BJIGT7MPr3fdKlEhiZHvs2',
+      alt: 'Gunpowder'
+    },
+    {
+      name: 'rockets',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbVhWPneS3LNGSwxmtn5fsrRdEyAvuqIkV6WiB',
+      alt: 'Rockets'
+    },
+    {
+      name: 'satchel',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbBfQYpZb80ShTgVK3Biu7bwx1QH9OzY6nNdsX',
+      alt: 'Satchel Charge'
+    },
+    {
+      name: 'sulfur',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbM9djIeetFkzwbAr2lMcOV7xtjQ5GvgoXIJiH',
+      alt: 'Sulfur'
+    }
+  ],
+  resources: [
+    {
+      name: 'cloth',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbBgqV7Mb80ShTgVK3Biu7bwx1QH9OzY6nNdsX',
+      alt: 'Cloth'
+    },
+    {
+      name: 'dieselbarrel',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb82XwhhP3mSlEaoq2dGyXu9hcJfQnxHp1b8LF',
+      alt: 'Diesel Barrel'
+    },
+    {
+      name: 'hq',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbD0NubprkPn9ZDaz2WwLGKMOiIS3XjETCmq6R',
+      alt: 'High Quality Metal'
+    },
+    {
+      name: 'hqore',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb3kcqFZi9P6xWUVrEduMO0w7TRalImANSG23g',
+      alt: 'High Quality Metal Ore'
+    },
+    {
+      name: 'metalfragment',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbPvplUaOU8zq5OiBgAwQIHeXsxGSkZTjrnF6D',
+      alt: 'Metal Fragment'
+    },
+    {
+      name: 'scrap',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbMKqXZotFkzwbAr2lMcOV7xtjQ5GvgoXIJiH0',
+      alt: 'Scrap'
+    },
+    {
+      name: 'stone',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbS06c0mW2zknewFTfKcDlpbrZxX4VG6tRuo8m',
+      alt: 'Stone'
+    },
+    {
+      name: 'sulfurore',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbBqzPnkb80ShTgVK3Biu7bwx1QH9OzY6nNdsX',
+      alt: 'Sulfur Ore'
+    }
+  ],
+  walls: [
+    {
+      name: 'hqwall',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb5eq4lkAWVw26sdcoTNqvRPXOHlbKIygxDAMr',
+      alt: 'High Quality Wall'
+    },
+    {
+      name: 'metalwall',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwban92XWvuCF7dwP2ilOLhc6MIQtH3Wq81UaAB',
+      alt: 'Metal Wall'
+    },
+    {
+      name: 'reinforcedglasswindow',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb6wMuW7zoBUeFNDc54MhWR8YEuX6nCxftIGjz',
+      alt: 'Reinforced Glass Window'
+    },
+    {
+      name: 'shopfrontmetal',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbU5VW13DX40uBwLHGjA9OWQxRVm5YbEyz1cJI',
+      alt: 'Metal Shop Front'
+    },
+    {
+      name: 'stonewall',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb2yuduJUE0eMyUA5kNuQ7qFdmf9LOCKGB1Ziv',
+      alt: 'Stone Wall'
+    },
+    {
+      name: 'stonewallexternalhigh',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb4O1WlTRQ81ETnxS0sWdfDHYal5wgpBZMV6tq',
+      alt: 'High External Stone Wall'
+    },
+    {
+      name: 'windowglassreinforced',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbyeQ6pDPK3pmj0QMf1IDoPi5sAvzYF8xEJrt9',
+      alt: 'Reinforced Glass Window'
+    },
+    {
+      name: 'woodenwall',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwbSkcXwxW2zknewFTfKcDlpbrZxX4VG6tRuo8m',
+      alt: 'Wooden Wall'
+    },
+    {
+      name: 'woodenwallexternalhigh',
+      url: 'https://1xoi7lid5j.ufs.sh/f/MyoBRstFkzwb2sEqQWUE0eMyUA5kNuQ7qFdmf9LOCKGB1Ziv',
+      alt: 'High External Wooden Wall'
+    }
+  ]
+};
